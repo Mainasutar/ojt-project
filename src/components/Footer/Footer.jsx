1fa08ca62831339d21448f5c3e22b5de45296b20
Maina <mainasutar25@gmail.com>
2025-06-13 12:09:41 +0530

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,14 @@
 // src/components/pages/Footer.jsx
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer className="app-footer">
-            <p>&copy; 2025 Vivekanand College - All rights reserved</p>
-        </footer>
+        <div>
+            <footer className="app-footer">
+                <p>&copy; 2025 Vivekanand College - All rights reserved</p>
+            </footer>
+        </div>
     );
 };
 
